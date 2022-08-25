0c2bf1d24ccc749af515fc6c7d64c88817ec9ce1
Skussauer <112044437+Skussauer@users.noreply.github.com>
2022-08-24 21:06:50 -0700

    Update CMJ_FVP_final.py

--- CMJ_FVP_final.py
+++ CMJ_FVP_final.py
@@ -7,7 +7,7 @@ from nptdms import TdmsFile
 from scipy import integrate
 from scipy import signal
 from glob import glob 
-from scandir import os
+from os import scandir
 from pathlib import Path
 import matplotlib.pyplot as plt
 
